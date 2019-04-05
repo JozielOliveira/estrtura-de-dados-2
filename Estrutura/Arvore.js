2130154e4de0f4c0c36e982e35680e2149c99313
Joziel Oliveira Santos <programdor.joziel@gmail.com>
2019-04-05 10:49:56 -0300

    Update Arvore.js

--- Estrutura/Arvore.js
+++ Estrutura/Arvore.js
@@ -1,52 +1,54 @@
 class Tree { 
     
     constructor(){
-        this.item = null
+        this.valor = null
         this.direita = null
         this.esquerda = null
     }
 
-    insert (item) {
-        if (this.item == null) {// verifiaca se há elemento na raiz
-            this.item = item
-        } else if (item < this.item){
+    insert (valor) {
+        if (this.valor == null) {
+            this.valor = valor
+        } else if (valor < this.valor){
             if (this.esquerda == null)
                 this.esquerda = new Tree()
-            return this.esquerda.insert(item)
+            return this.esquerda.insert(valor)
         } else{
             if (this.direita == null)
                 this.direita = new Tree()
-            return this.direita.insert(item)
+            return this.direita.insert(valor)
         }
         
     }
 
-    search (item, next) {
-        if (this.item == item)
+    search (valor, next) { 
+        if (this.valor == valor)
+            // Se não haver um proxima função a ser executada retorna true
+            // Se haver um proxima funcão, passa como parametro o contexto em que foi encotrado o valor
             return next ? next(this) : true
-        else if (item < this.item && this.esquerda != null)
-            return this.esquerda.search(item, next)
+        else if (valor < this.valor && this.esquerda != null)
+            return this.esquerda.search(valor, next)
         else if (this.direita != null)
-            return this.direita.search(item, next)
+            return this.direita.search(valor, next)
         else
             return false
     }
         
     inOrdem () {
-        if (this.item != null)
+        if (this.valor != null)
             this.ordem("inOrdem")
     }
     
     posOrdem () {
-        if (this.item != null) {
+        if (this.valor != null) {
             this.ordem("posOrdem")
-            console.log(this.item)// Raiz
+            console.log(this.valor)// Raiz
         }
     }
 
     preOrdem () {
-        if (this.item != null) {
-            console.log(this.item)// Raiz
+        if (this.valor != null) {
+            console.log(this.valor)// Raiz
             this.ordem("preOrdem")
         }
     }
@@ -55,43 +57,59 @@ class Tree {
         if (this.esquerda != null)
             this.esquerda[func]()//Esquerda
         if (func === "inOrdem") 
-            console.log(this.item)// Raiz
+            console.log(this.valor)// Raiz
         if (this.direita != null)
             this.direita[func]() //Direita
     }
                 
     regra1 () {
-        if (this.esquerda != null)
+        // Primeira regra é varificar se a raiz possue sub-arvores
+        if (this.esquerda != null) // Se tiver à esquerda
             return this.esquerda.regra2("direita") // Vai pra esquerda e procura o maior valor pra atribuir no lugar
-        else if (this.direita != null)
-            return this.direita.regra2("esquerda") // Vai pra esquerda e procura o menor valor pra atribuir no lugar
-        else
+        else if (this.direita != null) // Se tiver à direita
+            return this.direita.regra2("esquerda") // Vai pra direita e procura o menor valor pra atribuir no lugar
+        else // Se a raiz não tiver sub-arvores, ela vai ser anulada
             return null
     }
 
     regra2 (ladoSubstituido) {
-        const ladoSubstituto = ladoSubstituido !== "direita" ?  ladoSubstituido : "esquerda"
-
+        // Logica => se o valor que está sendo procurado(ladoSubstituido) for da direita
+        // logo o que pode tomar o lugar vai ser o da esquerda, e vise versa
+        const ladoSubstituto = ladoSubstituido != "direita" ?  "direita" : ladoSubstituido
+        
         if (this[ladoSubstituido] != null)
+            // Caso a variavel for ladoSubstituido = direita, 
+            // o algoritmo vai sempre pra direita procurando o maior
+            // pois o maior está à direita
             return this[ladoSubstituido].regra2(ladoSubstituido)
-        else if (this[ladoSubstituto] != null)
-            return this.swap(this,ladoSubstituto)
-        else 
+        else if (this[ladoSubstituto] != null) // Caso não ter mais sub-arvores à direita, verifica-se se há à esquerda
+            // Se tiver sub-arvore à esquerda, executa a troca de atributos, da esquerda, 
+            // para a arvore em contexto e retorna o valor do antigo contexto
+            return this.swap(this, ladoSubstituido, ladoSubstituto)
+        else
+            // Se tiver sub-arvore à esquerda, executa a anulação da sub-arvore em contexto,
+            // e retorna o valor do antigo contexto
             return this.swap(this)
 
     }
 
-    swap (ref, lado) {
-        const value = ref.item
-        ref.item = lado ? ref[lado].item : null
-        ref.direita = lado ? ref[lado].item : null
-        ref.esquerda = lado ? ref[lado].item : null
+    swap (ref, ladoSubstituido, ladoSubstituto) {
+        const value = ref.valor // Pega o valor do contexto a ser alterado
+        if (ladoSubstituido) { // Verifica se há alguma sub-arvore pare substituir
+            ref.valor = ref[ladoSubstituto].valor // Atribui ao valor em contexto, o valor da posição de substituição, exemplo esquerdo
+            ref[ladoSubstituido] = ref[ladoSubstituto][ladoSubstituido]// Atribui ao lado(e/d) em contexto, o valor da posição de substituiçãovalor, exemplo esquerdo
+            ref[ladoSubstituto] = ref[ladoSubstituto][ladoSubstituto] ? ref[ladoSubstituto][ladoSubstituto] : null // Atribui ao lado(e/d) em contexto, o valor da posição de substituiçãovalor, exemplo direito
+        } else {
+            ref.valor = ref[ladoSubstituido] = ref[ladoSubstituto] = null // Se não tiver sub-arvore de substituição, anula a sub-arvore em contexto
+        }
+        // retorna o da sub-arvore valor antes de ser alterado
         return value
     }
 
-    delete (item) {
-        // Chama o metodo de procura, e com a referencia de onde foi encontrada o valor/item
-        this.search(item, ref => ref.item = ref.regra1() )// Aplica a primeira regra
+    delete (valor) {
+        // Chama o metodo de procura, e com a referencia de onde foi encontrada o valor
+        // Aplica a primeira regra no contexto em que foi entrado o valor
+        this.search(valor, ref => ref.valor = ref.regra1() )
     }
 }
     
@@ -110,7 +128,6 @@ arvore.insert(14)
 arvore.insert(15)
 arvore.insert(8)
 arvore.insert(9)
-
 // Mostrar em preOrdem
 console.log("Mostrar em preOrdem")
 arvore.preOrdem()
@@ -129,5 +146,4 @@ console.log("Delete 10")
 arvore.delete(10)
 console.log("Mostrar em preOrdem")
 arvore.preOrdem()
-
 
